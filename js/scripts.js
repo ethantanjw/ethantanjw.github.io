4dd0a00f4eed12a847bfef898de4dfef9f47bbd7
ethantanjw <ethantan123@gmail.com>
2023-06-27 00:13:01 +0800

    Second commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function openPDF() {
+    window.open('EthanTanJunWei_Resume.pdf', '_blank');
+  }
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+  
